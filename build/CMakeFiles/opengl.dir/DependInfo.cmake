e8ff7fcf925c6f3d6a89cc9ce3f4c02002e9859d
Mateusz Król <mateuszkrol221@gmail.com>
2024-04-23 02:15:19 +0200

    configured opengl project

---
+++ build/CMakeFiles/opengl.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mateusz-krol/Desktop/repos/opengl/display.cpp" "CMakeFiles/opengl.dir/display.cpp.o" "gcc" "CMakeFiles/opengl.dir/display.cpp.o.d"
+  "/home/mateusz-krol/Desktop/repos/opengl/main.cpp" "CMakeFiles/opengl.dir/main.cpp.o" "gcc" "CMakeFiles/opengl.dir/main.cpp.o.d"
+  "/home/mateusz-krol/Desktop/repos/opengl/shader.cpp" "CMakeFiles/opengl.dir/shader.cpp.o" "gcc" "CMakeFiles/opengl.dir/shader.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/opengl.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl.dir/display.cpp.o"
+  "CMakeFiles/opengl.dir/display.cpp.o.d"
+  "CMakeFiles/opengl.dir/main.cpp.o"
+  "CMakeFiles/opengl.dir/main.cpp.o.d"
+  "CMakeFiles/opengl.dir/shader.cpp.o"
+  "CMakeFiles/opengl.dir/shader.cpp.o.d"
+  "opengl"
+  "opengl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
